75c3d5f154e39a33e2a3d9d32122b3467faaef28
AsipheFunda <Asiphefunda@gmail.com>
2023-03-13 12:00:07 -0900

    UserTest edited

--- src/test/java/za/ac/cput/UserTest.java
+++ src/test/java/za/ac/cput/UserTest.java
@@ -2,9 +2,7 @@
 
 package za.ac.cput;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,20 +11,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
 
     @Test
-    public void test() {
-        User u = new User("User102", "User_password");
-        System.out.println(u);
+    @Disabled
+    public void testObjectEquality() {
+        User u1 = new User("123","password");
+        User u2 = new User("123","password");
+        assertEquals(u1,u2);
+    }
+    @Test
+    public void testObjectIdentity(){
+        User u1 = new User("123","password");
+        User u2 = new User("123","password");
+        assertNotSame(u1, u2);
+    }
+
+    @Test
+    public void testInequality(){
+        User u1 = new User("123","password");
+        User u2 = new User("52","My login");
+        assertNotEquals(u1, u2);
+    }
+    @Test
+    @Timeout(50)
+    public void testTimeout(){
+        User user = new User("123","password");
+        for(int i = 0; i<500000000; i++){
+            user.setLoginId("User"+1);
+        }
     }
     @Test
     public void test_fail(){
-        User u = User.createUser( "", "User_password");
-        System.out.println(u.toString());
-        assertNotNull(u);
-    }
-    @Test
-    public void timeout() throws InterruptedException {
-
-        TimeUnit.SECONDS.sleep(5);
-
+        fail("This test fails because someone was lazy");
+        //User u = User.createUser( "", "User_password");
+        //System.out.println(u.toString());
+        //assertNotNull(u);
     }
 }
